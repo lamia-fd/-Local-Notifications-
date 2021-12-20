cb20b145ad6aba2e85a87a6fc8271dcb36642fd6
lamia <lamia.fd123@gmail.com>
2021-12-20 12:05:35 +0300

    Initial Commit

---
+++ Local Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Local Notifications
+//
+//  Created by لمياء فالح الدوسري on 16/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
